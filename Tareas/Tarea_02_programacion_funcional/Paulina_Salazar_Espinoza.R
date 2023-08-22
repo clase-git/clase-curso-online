d18cbf0c9997405c5f9a54530a9aaf165c1a12e8
PaulinaSalaza <pasalazare@ine.gob.cl>
2023-08-22 10:28:52 -0400

    Tarea 2

--- Tareas/Tarea_02_programacion_funcional/Paulina_Salazar_Espinoza.R
+++ Tareas/Tarea_02_programacion_funcional/Paulina_Salazar_Espinoza.R
@@ -32,63 +32,8 @@ plots <- plot_with_for(gapminder_list)
 
 
 
+#Ejercicio 1
 
-
-# Definir la función plot_with_purrr para la población mundial según continente
-plot_with_purrr <- function(year_list) {
-  # Mapear sobre la lista de años y generar un gráfico para cada año
-  plot_list <- map(year_list, function(year) {
-    year_data <- gapminder[gapminder$year == year, ]
-    continent_pop <- tapply(year_data$pop, year_data$continent, sum)
-    pop_df <- data.frame(continent = names(continent_pop), population = continent_pop)
-    
-    ggplot(data = pop_df, aes(x = continent, y = population, fill = continent)) +
-      geom_bar(stat = "identity") +
-      labs(title = paste("Población mundial, según continente. Año -", year),
-           x = "Continente",
-           y = "Población",
-           fill = "Continente") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotar etiquetas del eje x
-    # Puedes personalizar el gráfico según tus necesidades
-  })
-  
-  return(plot_list)
-}
-
-# Obtener la lista de años únicos en la base de datos gapminder
-years <- unique(gapminder$year)
-
-# Generar la lista de gráficos utilizando la función
-graficos <- plot_with_purrr(years)
-
-# Imprimir los gráficos (puedes guardarlos o visualizarlos según tus necesidades)
-print(graficos)
-
-
-
-
-# Crear la lista de tablas por año
-gapminder_list <- split(gapminder, gapminder$year)
-
-# Función para generar los gráficos utilizando purrr
-plot_with_purrr <- function(tablas) {
-  map(tablas, function(plot) {
-    table <- sum_something(plot, continent, pop)
-    input_title <- paste("Población mundial, según continente. Año", plot$year[1])
-    plot_table(table, continent, n, input_title)
-  })
-}
-
-# Generar los gráficos utilizando la función plot_with_purrr
-plots <- plot_with_purrr(gapminder_list)
-
-# Imprimir los gráficos (puedes guardarlos o visualizarlos según tus necesidades)
-print(plots)
-
-
-gapminder_list <- split(gapminder, gapminder$year)
-
-# Función para generar los gráficos utilizando purrr::imap
 plot_with_purrr <- function(tablas) {
   imap(tablas, ~ {
     table <- sum_something(.x, continent, pop)
@@ -97,15 +42,13 @@ plot_with_purrr <- function(tablas) {
   })
 }
 
-# Generar los gráficos utilizando la función plot_with_imap
 plots <- plot_with_purrr(gapminder_list)
 
-# Imprimir los gráficos (puedes guardarlos o visualizarlos según tus necesidades)
 print(plots)
 
 
 
-
+#Ejercicio 2
 
 # Función para crear el gráfico con subtítulo
 plot_table <- function(table, x_var, y_var, title, subtitulo) {
@@ -114,17 +57,14 @@ plot_table <- function(table, x_var, y_var, title, subtitulo) {
     labs(title = title, subtitle = subtitulo)
 }
 
-# Función para resumir datos
 sum_something <- function(data, group_var, var) {
   data %>%
     group_by(!!enexpr(group_var)) %>%
     summarise(n = sum(!!enexpr(var)))
 }
 
-# Crear la lista de tablas por año
 gapminder_list <- split(gapminder, gapminder$year)
 
-# Función para generar los gráficos utilizando purrr::imap
 plot_with_purrr <- function(tablas) {
   imap(tablas, ~ {
     table <- sum_something(.x, continent, pop)
@@ -134,10 +74,28 @@ plot_with_purrr <- function(tablas) {
   })
 }
 
-# Generar los gráficos utilizando la función plot_with_purrr
 plots <- plot_with_purrr(gapminder_list)
 
-# Imprimir los gráficos (puedes guardarlos o visualizarlos según tus necesidades)
 print(plots)
+
+#Ejercicio 3
+
+nested_for <- function(v1, v2) {
+  for (x in v1) {
+    for (y in v2){
+      print(paste(x, y))
+    }
+  }
+}
+nested_for(1:3, 5:8)
+
+library(purrr)
+
+nested_map <- function(v1, v2) {
+  map(v1, ~ map(v2, ~ print(paste(.x, .))))
+}
+
+nested_map(1:3, 5:8)
+
 
 
