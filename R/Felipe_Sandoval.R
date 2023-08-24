9042d7295e430bd0d321a3497d40d6e578379300
Fsandovals <fsandovals@udec.cl>
2023-08-23 23:25:19 -0400

    nada

--- R/Felipe_Sandoval.R
+++ R/Felipe_Sandoval.R
@@ -1,2 +1,12 @@
 ##Prueba - Felipe Sandoval
 ##Agregando cambios
+
+##codigo sesion 1
+library(dplyr)
+library(tidyverse)
+
+df <- dplyr::tibble(x=list(1,2:3,4:6))
+df %>% dplyr::mutate(largo = map_int(x,length))
+df %>% rowwise() %>% mutate(largo=length(x))
+
+
