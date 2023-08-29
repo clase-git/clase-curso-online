9fe61a25ffcb494e639b6b244b5b144b14d40cca
Jaime Cristian Tapia Diaz <jctapiad@ine.gob.cl>
2023-08-29 10:25:24 -0400

    me equivoqué de carpeta, borro tareas de carpetas 3 (que era la tarea 2) y ahora muevo tarea de carpeta 4 a carpeta 3, como corresponde. Sry.

--- Tareas/Tarea_03_exp_reg/Jaime_Tapia_Diaz.R
+++ Tareas/Tarea_03_exp_reg/Jaime_Tapia_Diaz.R
@@ -1,85 +1,55 @@
-library(gapminder)
-library(dplyr)
-library(ggplot2)
-library(rlang)
-library(purrr)
+install.packages("stopwords")
+install.packages("qdap")
+install.packages("tm")
+
+library(feather)
+library(tidyverse)
 
 
-#---------------EJERCICIO 1------------------
+casen = read_feather("casen_2020_edit.feather")
+casen <- head(casen,100) # para un procesamiento liviano 
 
-# crea función que recibe una tabla, variable para agrupar, variable a sumar y el año
-sum_something <- function(data, group_var, var, year) {
-  data %>% 
-    group_by(!!enexpr(group_var), ano =!!enexpr(year)) %>% 
-    summarise(n = sum(!!enexpr(var)))
-}
+################## Ejercicio 1 ##################
 
-# función recibe tabla, var eje x, var eje y, texto para titulo 
-plot_table <- function(table, x_var, y_var, input_title ) {
-  ggplot(table, aes(x = !!enexpr(x_var), y = !!enexpr(y_var) )) +
-    geom_bar(stat = "identity") +
-    labs(title = input_title)
-}
+# opción 1
+mod_ocup_vivd_1 <- casen %>%
+                select(
+                        grep("^o\\d+", colnames(casen)), # selecciona todas que comienzan con 'o' y le sigue uno o más números
+                        grep("^v\\d+", colnames(casen)) # selecciona todas que comienzan con 'v' y le sigue uno o más números
+                        )
 
-# función recibe lista tabla para aplica a cada uno sum_something y plot_table por medio de map
-plot_with_purrr <- function(tablas){  
-  plots <- tablas %>% 
-  map(~sum_something(.x, continent, pop, year)) %>% 
-  map(~plot_table(.x, continent, n, paste("Población mundial, según continente -", .x$ano[1])))
-  return(plots)
-  }
+# opción 2
+mod_ocup_vivd_2 <- casen %>%
+                select(
+                        which(str_detect(colnames(casen), "^o\\d+")),  # selecciona todas que comienzan con 'o' y le sigue uno o más números
+                        which(str_detect(colnames(casen), "^v\\d+"))  # selecciona todas que comienzan con 'v' y le sigue uno o más números
+                        )
+  
+  
+################## Ejercicio 2 ##################
 
-# separa el data gapminder en listas según año
-gapminder_list <- split(gapminder, gapminder$year)
+library(stopwords)
+library(qdap)
+library(tm)
 
-# aplica función plot_with_purrr con gapminder_list
-plot_with_purrr(gapminder_list)
-
-
-
-#-----------------EJERCICIO 2------------------------
-
-# crea función que recibe una tabla, variable para agrupar, variable a sumar y el año
-sum_something <- function(data, group_var, var, year) {
-  data %>% 
-    group_by(!!enexpr(group_var), ano =!!enexpr(year)) %>% 
-    summarise(n = sum(!!enexpr(var)))
-}
-
-# función recibe tabla, var eje x, var eje y, texto para titulo y el año para el subtitulo
-plot_table <- function(table, x_var, y_var, input_title, ano ) {
-  ggplot(table, aes(x = !!enexpr(x_var), y = !!enexpr(y_var) )) +
-    geom_bar(stat = "identity") +
-    labs(title = input_title, subtitle = ano)
-}
-
-# función recibe lista tabla para aplica a cada uno sum_something y plot_table por medio de map
-plot_with_purrr <- function(tablas){  
-  plots <- tablas %>% 
-    map(~sum_something(.x, continent, pop, year)) %>% 
-    map(~plot_table(.x, continent, n, "Población mundial, según continente.", .x$ano[1]))
-  return(plots)
-}
-
-# separa el data gapminder en listas según año
-gapminder_list <- split(gapminder, gapminder$year)
-
-# aplica función plot_with_purrr con gapminder_list
-plot_with_purrr(gapminder_list)
+procesar <- function(var){
+  var <- var %>%
+        tolower() %>%                         # convierte a minúsculas
+        str_replace_all("[:punct:]","") %>%   # quita caracteres de puntuación y especiales:!»#$%&‘()*+,–./:;<=>?@[\]^_`{|}~
+        str_remove_all("[0-9]") %>%           # remueve números
+        str_replace_all("\\s{2,}"," ") %>%    # reemplaza los espacios dobles o más por solo uno
+        str_remove_all("^\\s|\\s$")      # remueve los espacios de inicio y final
+  
+  var <- rm_stopwords(var, stopwords("es"))[] # remueve los stopwords en español
+  var <- map(var,~paste(.x, collapse = " ")) # reúne cada palabra en una cadena de cada elemento de la lista
+  
+  return(var)
+          }
 
 
-
-#-----------------EJERCICIO 3------------------------
-
-# función recibe 2 vectores, lo transforma en lista y realiza las combinaciones entre sus elementos y luego imprime
-nested_map <- function(v1, v2) {
-  data <- list( v2, v1 )
-  walk(data %>%
-        cross() %>%
-        map(lift(paste)), print)
-}
-
-nested_map(1:3, 5:8)
-
-
+test <- tibble(                          # probando función
+                procesar(casen$o9a),
+                procesar(casen$o9b),
+                procesar(casen$o24)
+                )
 
